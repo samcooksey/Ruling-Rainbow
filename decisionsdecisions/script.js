09a6b48107d89a1bece2f6bd4d3eabe35fa20ff5
Samuel Cooksey <slamcooksey@gmail.com>
2016-05-24 09:43:18 -0600

    saving current version before adding save options

--- decisionsdecisions/script.js
+++ decisionsdecisions/script.js
@@ -48,14 +48,21 @@ var chooseButton = document.querySelector("#choose");
 var resultHolder = document.querySelector("#result");
 var saveButton = document.querySelector("#save");
 var loadButton = document.querySelector("#load");
-var result = document.createElement("div");
+var selectedOptionBox = document.querySelector("#selectedOption");
+var answer = document.querySelector("#answer");
+var answerImg = document.querySelector("#answerImg");
+
 
 var weightCounter = 0;
 var activeWeightCounters = [];
 
 
 
-chooseButton.addEventListener("click", function(){
+selectedOptionBox.addEventListener("click", function(){
+  if(Object.keys(options).length <= 1 ){
+    answer.style.fontSize = ".5em";
+    answer.innerHTML = "I need at least 2 options to decide!";
+  }else{
   randomNumber(activeWeightCounters.length-1, function(data){
     var chosenOption;
     for(var key in options){
@@ -67,24 +74,36 @@ chooseButton.addEventListener("click", function(){
         }
       }
     }
-
-    result.innerHTML = chosenOption;
-    resultHolder.appendChild(result);
+    // answerImg.src = "http://media1.giphy.com/media/I2sZ6qckkhfLG/giphy.gif";
+    answer.innerHTML = "";
+    // answer.style.color = "white";
+    // setTimeout(function(){answer.innerHTML = chosenOption;}, 2000);
+    answer.innerHTML = chosenOption;
+    // answer.style.animationName = "zoom";
+    console.log("active counters " + activeWeightCounters);
+    console.log("data " + data);
+    console.log("chosen Option " + chosenOption);
+    console.log("option weight array " + options[chosenOption].weight);
   });
+}
 });
 var submit = function(){
-  var option = document.createElement("div");
-  option.innerHTML = optionName.value;
-  options[optionName.value] = {};
-  options[optionName.value].weight = [];
-  for (var i = 0; i <10; i ++){
-    options[optionName.value].weight[i] = weightCounter;
-    activeWeightCounters.push(weightCounter);
-    weightCounter++;
+  if(optionName.value !== ""){
+    var option = document.createElement("div");
+    option.innerHTML = optionName.value;
+    options[optionName.value] = {};
+    options[optionName.value].weight = [];
+    for (var i = 0; i <10; i ++){
+      options[optionName.value].weight[i] = weightCounter;
+      activeWeightCounters.push(weightCounter);
+      weightCounter++;
+
+    }
+    document.querySelector("#space" + String(Object.keys(options).length-1)).appendChild(option);
+    option.parentNode.style.border = "1px solid black";
+    optionName.value = '';
+    optionName.focus();
   }
-  optionList.appendChild(option);
-  optionName.value = '';
-  optionName.focus();
 };
 submitButton.addEventListener("click", submit);
 optionName.addEventListener("keyup", function(){
@@ -103,11 +122,13 @@ loadButton.addEventListener("click", function(){
   activeWeightCounters = JSON.parse(localStorage.activeWeightCounters);
   weightCounter = JSON.parse(localStorage.weightCounter);
   options = JSON.parse(localStorage.options);
-  console.log(options);
+  var spaceCounter = 0;
   for (var key in options){
     var option = document.createElement("div");
     option.innerHTML = key;
-    optionList.appendChild(option);
+    document.querySelector("#space" + String(spaceCounter)).appendChild(option);
+    option.parentNode.style.border = "1px solid black";
+    spaceCounter++;
   }
 });
 
